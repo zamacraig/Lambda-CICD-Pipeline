29fd59f57951deb315e91ecde218e3af39940194
Zama Dhladhla <zamacraig@gmail.com>
2025-02-05 09:46:47 +0200

    Initial commit

---
+++ src/LambdaCicdPipeline/LambdaCicdPipelineStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace LambdaCicdPipeline
+{
+    public class LambdaCicdPipelineStack : Stack
+    {
+        internal LambdaCicdPipelineStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
